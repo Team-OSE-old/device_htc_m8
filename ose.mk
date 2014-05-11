737f788e07fce4e79e22f56e847546bdb2514bf1
Tim Pohlad-Thomas <timpohladthomas@gmail.com>
2014-05-11 18:45:43 -0400

    Update and rename liquid.mk to ose.mk

---
+++ ose.mk
@@ -0,0 +1,9 @@
+$(call inherit-product, device/htc/m8/full_m8.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/ose/config/nfc_enhanced.mk)
+
+# Inherit some common Liquid stuff.
+$(call inherit-product, vendor/ose/config/common_phone.mk)
+
+PRODUCT_NAME := ose_m8
